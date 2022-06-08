b62ac5e8b27dc0212c6c0dc0a2a149e93bd5f198
JH-debug <oreomasida@gmail.com>
2022-06-08 20:31:47 +0900

    fix layer and test

--- audio/reference_encoder_vae.py
+++ audio/reference_encoder_vae.py
@@ -9,7 +9,6 @@ class VAE_StyleTokenLayer(nn.Module):
         super().__init__()
         output_dim = gru_units // 2
 
-        self.ref_encoder = ReferenceEncoder()
         self.fc1 = nn.Linear(gru_units, z_latent_dim)
         self.fc2 = nn.Linear(gru_units, z_latent_dim)
         self.fc_final = nn.Linear(z_latent_dim, output_dim)
@@ -22,13 +21,33 @@ class VAE_StyleTokenLayer(nn.Module):
         else:
             return mu
 
-    def forward(self, inputs):
-        encoder_out = self.ref_encoder(inputs)
-        mu = self.fc1(encoder_out)
-        logvar = self.fc2(encoder_out)
+    def forward(self, ref_embeddings):
+        mu = self.fc1(ref_embeddings)
+        logvar = self.fc2(ref_embeddings)
         z = self.reparameterize(mu, logvar)
         style_embed = self.fc_final(z)
 
         return style_embed, mu, logvar, z
 
 
+if __name__ == "__main__":
+    from dataloader import *
+
+    MTAT_DIR = '../dataset/MTAT_SMALL/'
+    BATCH_SIZE = 16
+    NUM_MAX_DATA = 50
+
+    train_data_loader = create_data_loader(MTAT_DIR, 'train', NUM_MAX_DATA, BATCH_SIZE)
+    example = next(iter(train_data_loader))
+    print(example[0].shape)
+
+    mel_spec = MelSpectogram()
+    mel_spec_out = mel_spec(example[0])
+    print(mel_spec_out.shape)
+
+    reference_encoder = ReferenceEncoder(idim=911)
+    ref_embed = reference_encoder(mel_spec_out)
+    print(ref_embed.shape)
+
+    style_token = VAE_StyleTokenLayer()
+    print(style_token(ref_embed)[0].shape)

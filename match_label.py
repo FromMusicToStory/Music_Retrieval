1267909d1a455a3232d82ea6f304d2679fe3c52a
JH-debug <oreomasida@gmail.com>
2022-06-16 11:08:30 +0900

    label matching (7 labels to 5 labels)

--- match_label.py
+++ match_label.py
@@ -2,7 +2,8 @@ import json
 from collections import defaultdict
 
 
-MTAT_tag = ['singer', 'harpsichord', 'sitar', 'heavy', 'foreign',
+## Music Dataset tag label
+MTAT = ['singer', 'harpsichord', 'sitar', 'heavy', 'foreign',
        'no piano', 'classical', 'female', 'jazz', 'guitar', 'quiet', 'solo',
        'folk', 'ambient', 'new age', 'synth', 'drum', 'bass', 'loud', 'string',
        'opera', 'fast', 'country', 'violin', 'electro', 'trance', 'chant',
@@ -11,63 +12,71 @@ MTAT_tag = ['singer', 'harpsichord', 'sitar', 'heavy', 'foreign',
        'rock', 'dance', 'cello', 'techno', 'flute', 'beat', 'soft', 'choir',
        'baroque']
 
-Jamendo_mood = ["action","adventure","advertising","ambiental","background","ballad",
-                "calm","children","christmas","commercial","cool","corporate","dark","deep",
-                "documentary","drama","dramatic","dream","emotional","energetic","epic","fast",
-                "film","fun","funny","game","groovy","happy","heavy","holiday","hopeful","horror",
-                "inspiring","love","meditative","melancholic","mellow","melodic","motivational","movie",
-                "nature","party","positive","powerful","relaxing","retro","romantic","sad","sexy","slow",
-                "soft","soundscape","space","sport","summer","trailer","travel","upbeat","uplifting"]
+Jamendo = ["action","adventure","advertising","ambiental","background","ballad",
+           "calm","children","christmas","commercial","cool","corporate","dark","deep",
+           "documentary","drama","dramatic","dream","emotional","energetic","epic","fast",
+           "film","fun","funny","game","groovy","happy","heavy","holiday","hopeful","horror",
+           "inspiring","love","meditative","melancholic","mellow","melodic","motivational","movie",
+           "nature","party","positive","powerful","relaxing","retro","romantic","sad","sexy","slow",
+           "soft","soundscape","space","sport","summer","trailer","travel","upbeat","uplifting"]
 
 
-label = defaultdict(list)
+def make_label():
 
-label['MTAT']
-label["Jamendo"]
+    label = defaultdict(list)
 
+    ## Positive
+    # 1. happy
+    happy = defaultdict(list)
+    happy['positive'] = ["dance", "techno", "pop", "action", "happy", "fun", "funny", "game", "groovy",
+                         "holiday", "energetic" "positive", "children", "upbeat", "uplifting", "christmas",
+                         "party", "travel", "summer"]
 
-## 1. happy
-# neg: 나머지 다
-happy = defaultdict(list)
-happy['positive'] = ["dance", "techno", "pop", "action", "happy", "fun", "funny", "game",
-                     "groovy", "holiday", "energetic" "positive", "children", "upbeat", "uplifting", "christmas", "party",
-                     "travel", "summer"]
-#happy["negative"] = ["dark", "horror", "sad", "ballad", "melancholic"]
+    # 2. neutral
+    neutral = defaultdict(list)
+    neutral["positive"] = ["calm", "ambiental", "ambient", "ballad", "background", "new age", "classical",
+                           "relaxing", "soft", "nature", "meditative", "soundscape", "quiet"]
 
+    # 3. flustered (flustered + anxious)
+    flustered = defaultdict(list)
+    flustered['positive'] = ["strange", "melancholic", "drama", "hopeful", "inspiring", "melodic"]
 
-## 2. flustered + anxious = flustered
-# neg: happy, neutral
-flustered = defaultdict(list)
-flustered['positive'] = ["strange", "adventure", "fast"]
-#flustered["negative"] = ["happy", "cool", "fun", ]
+    # 4. angry
+    angry = defaultdict(list)
+    angry["positive"] = ["heavy", "dark", "loud", "rock", "powerful", "beat", ]
 
-anxious = defaultdict(list)
-anxious["positive"] =  ["hopeful"]
-#anxious["negative"] =  []
-
-## 3. neutral
-# neg: 나머지 다
-neutral = defaultdict(list)
-neutral["positive"] =  ["calm", "ambiental", "ambient", "ballad", "background", "new age", "classical",
-                        "relaxing", "soft", "nature", "meditative", "soundscape"]
-#neutral["negative"] =  ["action", "drama", "dramatic", "emotional", "energetic", "fast", "game", "sport"]
-
-## 4. angry
-# neg: happy, neutral, sad
-angry = defaultdict(list)
-angry["positive"] =  ["heavy", "dark", "loud", "heavy", "rock", "powerful", "beat"]
-#angry["negative"] =  []
+    # 5. sad (hurt + sad)
+    sad = defaultdict(list)
+    sad["positive"] = ["string", "sad", "deep", "ballad", "emotional", "slow"]
 
 
-## 5. hurt + sad = sad
-# neg: happy, angry
-hurt = defaultdict(list)
-hurt["positive"] =  ["string", "sad", "melancholic"]
-#hurt["negative"] =  []
+    ## Negative
+    # 1. happy - neg: flustered, angry, sad
+    happy["negative"] = [i for i in (flustered['positive'] + angry['positive'] + sad['positive']) if i not in happy['positive']]
 
-sad = defaultdict(list)
-sad["positive"] =  ["deep", "ballad", "melancholic", "emotional", "sad", "slow"]
-#sad["negative"] =  []
+    # 2. neutral - neg: 나머지 다
+    neutral["negative"] = [i for i in (happy['positive'] + flustered['positive'] + angry['positive'] + sad['positive']) if i not in neutral['positive']]
+
+    # 3. flustered - neg: happy, neutral
+    flustered["negative"] = [i for i in (happy['positive'] + neutral['positive']) if i not in flustered['positive']]
+
+    # 4. angry - neg: happy, neutral, sad
+    angry["negative"] = [i for i in (happy['positive'] + neutral['positive'] + sad['positive']) if i not in angry['positive']]
+
+    # 5. sad - neg: happy, angry
+    sad["negative"] = [i for i in (happy['positive'] + angry['positive']) if i not in sad['positive']]
 
 
+    ## Final Label
+    label['happy'] = happy
+    label['neutral'] = neutral
+    label['flustered'] = flustered
+    label['angry'] = angry
+    label['sad'] = sad
+    #label['music'] = defaultdict(list, neutral=neutral)
 
+    return label
+
+
+label = make_label()
+print(label.keys())

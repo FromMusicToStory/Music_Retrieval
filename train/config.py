bde7451da26d3cb8ebf2c1babe9f96ea05e097a9
JH-debug <oreomasida@gmail.com>
2022-06-24 11:17:06 +0900

    update config file

--- train/config.py
+++ train/config.py
@@ -1,14 +1,14 @@
 metric_embedding_train_config = {
-    'audio_dir': '../dataset/mtg-jamendo-dataset/',
-    'text_dir': '../dataset/Story_dataset/',
+    'audio_dir': 'dataset/mtg-jamendo-dataset/',
+    'text_dir': 'dataset/Story_dataset/',
     'max_len': 512,
     'audio_max': 500,
-    'epochs': 10,
+    'epochs': 250,
     'batch_size': 8,
     'lr': 1e-4,
     'weight_decay': 1e-4,
     'accumulation_steps': 8,
     'cuda': 'cuda:0',
-    'log_dir': '../train/result/tensorboard',
-    'model_name': 'ml_with_embed_train_data'
+    'log_dir': 'train/result/tensorboard',
+    'model_name': 'ml_with_embed'
 }
